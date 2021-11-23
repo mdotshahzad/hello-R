07c723841bc2bca2f7b14ca895c9ed9bd3237eb9
Muhammad Shahzad <mdotshahzad@gmail.com>
2021-11-24 04:21:52 +0500

    New Introductry File added from RStudio to Github

---
+++ IntroFile.R
@@ -0,0 +1,8 @@
+#This is my introductory and test file which I upload from the RStudio to 
+#Github repo.
+#Name: Muhammad Shahzad
+#Title: Software Engineer {Technical Product Support} | Fintech & Payment Industry
+#Qualification: BS in Software Engineering
+#Session: 2015 - 2019
+#Experience: Data Scienctist @ Programmers Force 
+#Experience: Technical Product Support Engineer @ I2C INC. June 2020 to Present.
